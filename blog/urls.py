d72a7a6516e138e247473bda9f9f02cd743cc382
Gopal Prasad <gopalprasad780@gmail.com>
2017-05-04 00:47:00 +0530

    Added Comment Models, created database table, views of comment on post, add , remove approve comment functionality

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns=[
  url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
  url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
  url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name ='post_delete' ),
+ url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post' ),
+ url(r'^comment/(?P<pk>\d+)/approve/$', views.approve_comment, name='approve_comment'),
+ url(r'^comment/(?P<pk>\d+)/remove/$', views.remove_comment, name='remove_comment'),
 ]
